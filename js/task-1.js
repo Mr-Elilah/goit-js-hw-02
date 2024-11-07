7af2e629586efc84b2723471c9310d252243af50
Mr.Elijah <beztalannyj.ilia@gmail.com>
2024-11-07 14:47:24 -0800

    hw2

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,13 @@
-function makeTransaction(quantity, pricePerDroid) {
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
   const totalPrice = quantity * pricePerDroid;
 
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  return customerCredits >= totalPrice
+    ? `You ordered ${quantity} droids worth ${totalPrice} credits!`
+    : 'Insufficient funds!';
 }
 
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+console.log(makeTransaction(5, 3000, 23000));
+console.log(makeTransaction(3, 1000, 15000));
+console.log(makeTransaction(10, 5000, 8000));
+console.log(makeTransaction(8, 2000, 10000));
+console.log(makeTransaction(10, 500, 5000));
